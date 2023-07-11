37d359aa09f4ffee23c8848c7a523b0b209d6b82
Tushar Upadhyay <tusharupadhyay691@gmail.com>
2023-07-12 02:56:41 +0530

    add project

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,15 +1,14 @@
 import { useEffect } from "react";
 
-import resume from "../assets/images/projects/resumebuilder.png";
 import ecom from "../assets/images/projects/e-com.png";
 import pet from "../assets/images/projects/pet.png";
 import mi from "../assets/images/projects/mi.png";
 import youtube from "../assets/images/projects/youtube.png";
+import video from "../assets/images/projects/videcall.png";
+import admin from "../assets/images/projects/admin.png";
 
 import live from "../assets/images/icon/live.svg";
 import github from "../assets/images/icon/github.png";
-
-import { BsGithub } from "react-icons/bs";
 
 import gsap from "gsap";
 
@@ -116,21 +115,74 @@ const Projects = () => {
                 Featured Projects
             </h1>
             <div className="sm:space-y-40 space-y-32 xl:space-y-[35vh] z-30">
-                {/* E-COM */}
+                {/* ADMIN */}
+
+                <div className="container lg:space-x-2 flex flex-col lg:flex-row lg:justify-between lg:items-center items-start invisible">
+                    <div className="lg:h-[400px] 2xl:h-[500px] h-[250px] sm:h-[300px] md:h-[350px] w-full relative overflow-hidden img-wrapper">
+                        <img
+                            src={admin}
+                            alt="project image"
+                            className="absolute w-full h-full object-contain img"
+                        />
+                    </div>
+
+                    <div className="text-container text-white lg:space-y-4 space-y-2 md:space-y-3 sm:space-y-3">
+                        <h1 className="hidden-text cursor-default font-Glimer-Bold font-bold lg:text-4xl text-2xl sm:text-3xl 2xl:text-5xl lg:mt-0 mt-5">
+                            <div className="content-line flex lg:justify-end items-center m-0 lg:h-[52px] 2xl:h-[60px] overflow-hidden ">
+                                <div className="content-line-inner">
+                                    Admin DashBoard
+                                </div>
+                            </div>
+                        </h1>
+                        <p className="font-normal cursor-default font-Glimer-Light text-white/90 xl:text-2xl sm:text-xl 2xl:text-3xl vvs:text-base text-sm lg:text-right">
+                            An admin panel is focused on managing and updating a
+                            website or application, while a dashboard is focused
+                            on presenting data and insights.
+                        </p>
+                        <div className="btn-row flex justify-start space-x-6 items-center lg:justify-end">
+                            <div className="github-btn">
+                                <a
+                                    className="icon "
+                                    target="_blank"
+                                    href="https://github.com/tushar-upadhya/e-com-full"
+                                >
+                                    <img
+                                        src={github}
+                                        className="lg:h-12 lg:w-12 2xl:h-9 2xl:w-9 vvs:h-6 vvs:w-6 sm:w-7 sm:h-7 h-5 w-5 hover:-translate-y-1 transition duration-200  ease-in-out"
+                                    />
+                                </a>
+                            </div>
+                            <div className="liveDemo-btn">
+                                <a
+                                    className="icon"
+                                    target="_blank"
+                                    href="https://bummer-tusharupadhyay.vercel.app/"
+                                >
+                                    <img
+                                        src={live}
+                                        alt="live image"
+                                        className="link lg:h-12 lg:w-12 2xl:h-6 2xl:w-6 vvs:h-5 vvs:w-5 sm:w-6 sm:h-6 h-4 w-4 hover:-translate-y-1 transition duration-200 ease-in-out"
+                                    />
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                {/* VIDEO CALL */}
 
                 <div className="container lg:space-x-2 flex flex-col-reverse lg:flex-row lg:justify-between lg:items-center items-start invisible">
                     <div className="text-container text-white lg:space-y-4 space-y-2 md:space-y-3 sm:space-y-3 ">
                         <h1 className="hidden-text font-bold font-Glimer-Bold cursor-default lg:text-4xl text-2xl sm:text-3xl 2xl:text-5xl lg:mt-0 mt-5">
                             <div className="content-line flex items-center m-0 lg:h-[52px] 2xl:h-[60px] overflow-hidden ">
                                 <div className="content-line-inner">
-                                    E - Com
+                                    CHATVID
                                 </div>
                             </div>
                         </h1>
 
                         <p className="font-normal cursor-default font-Glimer-Light text-white/90 xl:text-2xl  sm:text-xl 2xl:text-3xl vvs:text-base text-sm">
-                            E-commerce website, Payment gateway and
-                            user-friendly shopping experience.
+                            CHATVID for real-time audio and video communication.
                         </p>
 
                         <div className="btn-row flex justify-start space-x-6 items-center">
@@ -163,21 +215,21 @@ const Projects = () => {
                         </div>
                     </div>
 
-                    <div className="lg:h-[400px] 2xl:h-[500px] h-[250px] sm:h-[300px] md:h-[350px] w-full relative overflow-hidden img-wrapper">
+                    <div className="lg:h-[500px] 2xl:h-[500px] h-[250px] sm:h-[300px] md:h-[350px] w-full relative overflow-hidden img-wrapper">
                         <img
-                            src={ecom}
+                            src={video}
                             alt="project image"
                             className="absolute w-full object-cover h-full img"
                         />
                     </div>
                 </div>
 
-                {/* resume Builder */}
+                {/* E-COM */}
 
                 <div className="container lg:space-x-2 flex flex-col lg:flex-row lg:justify-between lg:items-center items-start invisible">
                     <div className="lg:h-[400px] 2xl:h-[500px] h-[250px] sm:h-[300px] md:h-[350px] w-full relative overflow-hidden img-wrapper">
                         <img
-                            src={resume}
+                            src={ecom}
                             alt="project image"
                             className="absolute w-full h-full object-contain img"
                         />
@@ -187,21 +239,20 @@ const Projects = () => {
                         <h1 className="hidden-text cursor-default font-Glimer-Bold font-bold lg:text-4xl text-2xl sm:text-3xl 2xl:text-5xl lg:mt-0 mt-5">
                             <div className="content-line flex lg:justify-end items-center m-0 lg:h-[52px] 2xl:h-[60px] overflow-hidden ">
                                 <div className="content-line-inner">
-                                    Resume Builder
+                                    E - Com
                                 </div>
                             </div>
                         </h1>
                         <p className="font-normal cursor-default font-Glimer-Light text-white/90 xl:text-2xl sm:text-xl 2xl:text-3xl vvs:text-base text-sm lg:text-right">
-                            Users can customize their resumes with 5 color
-                            options and a drag-and-drop feature updates in
-                            real-time.
+                            E-commerce website, Payment gateway and
+                            user-friendly shopping experience.
                         </p>
                         <div className="btn-row flex justify-start space-x-6 items-center lg:justify-end">
                             <div className="github-btn">
                                 <a
                                     className="icon "
                                     target="_blank"
-                                    href="https://github.com/tushar-upadhya/resume-builder"
+                                    href="https://github.com/tushar-upadhya/e-com-full"
                                 >
                                     <img
                                         src={github}
@@ -213,12 +264,12 @@ const Projects = () => {
                                 <a
                                     className="icon"
                                     target="_blank"
-                                    href="https://resumebuilder-tusharupadhyay.vercel.app/"
+                                    href="https://bummer-tusharupadhyay.vercel.app/"
                                 >
                                     <img
                                         src={live}
                                         alt="live image"
-                                        className="link lg:h-8 lg:w-8 2xl:h-6 2xl:w-6 vvs:h-5 vvs:w-5 sm:w-6 sm:h-6 h-4 w-4 hover:-translate-y-1 transition duration-200 ease-in-out"
+                                        className="link lg:h-12 lg:w-12 2xl:h-6 2xl:w-6 vvs:h-5 vvs:w-5 sm:w-6 sm:h-6 h-4 w-4 hover:-translate-y-1 transition duration-200 ease-in-out"
                                     />
                                 </a>
                             </div>
