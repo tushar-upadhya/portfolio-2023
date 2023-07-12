34605603b675c48f330f6a6b20b60870fcc98cb9
Tushar Upadhyay <tusharupadhyay691@gmail.com>
2023-07-12 17:24:22 +0530

    resume

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
-import Footer from "./components/Footer";
 import Hero from "./components/Hero";
 import Projects from "./components/Projects";
 import TechStack from "./components/TechStack";
+import Footer from "./components/Footer";
 
 function App() {
     return (
