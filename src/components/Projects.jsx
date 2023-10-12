831b5e3891971ae922bc975c12b4ccbf3dffb9fe
Tushar Upadhyay <tusharupadhyay691@gmail.com>
2023-10-12 22:15:58 +0530

    add project

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -5,6 +5,7 @@ import mi from "../assets/images/projects/mi.png";
 import youtube from "../assets/images/projects/youtube.png";
 import admin from "../assets/images/projects/admin.png";
 import prm from "../assets/images/projects/readme.png";
+import beat from "../assets/images/projects/beat-x.png";
 
 import live from "../assets/images/icon/live2.png";
 import github from "../assets/images/icon/github.png";
@@ -114,63 +115,9 @@ const Projects = () => {
         Featured Projects
       </h1>
       <div className="sm:space-y-40 space-y-32 xl:space-y-[35vh] z-30 ">
-        {/* */}
-
-        {/* <div className="container lg:space-x-2 flex flex-col lg:flex-row lg:justify-between lg:items-center items-start invisible">
-                    <div className="lg:h-[400px] 2xl:h-[500px] h-[250px] sm:h-[300px] md:h-[350px] w-full relative overflow-hidden img-wrapper">
-                        <img
-                            src={admin}
-                            alt="project image"
-                            className="absolute w-full h-full object-contain img"
-                        />
-                    </div>
-
-                    <div className="text-container text-white lg:space-y-4 space-y-2 md:space-y-3 sm:space-y-3">
-                        <h1 className="hidden-text cursor-default font-Glimer-Bold font-bold lg:text-4xl text-2xl sm:text-3xl 2xl:text-5xl lg:mt-0 mt-5">
-                            <div className="content-line flex lg:justify-end items-center m-0 lg:h-[52px] 2xl:h-[60px] overflow-hidden ">
-                                <div className="content-line-inner">
-                                    Admin DashBoard
-                                </div>
-                            </div>
-                        </h1>
-                        <p className="font-normal cursor-default font-Glimer-Light text-white/90 xl:text-2xl sm:text-xl 2xl:text-3xl vvs:text-base text-sm lg:text-right">
-                            An admin panel is focused on managing and updating a
-                            website or application, while a dashboard is focused
-                            on presenting data and insights.
-                        </p>
-                        <div className="btn-row flex justify-start space-x-6 items-center lg:justify-end">
-                            <div className="github-btn">
-                                <a
-                                    className="icon "
-                                    target="_blank"
-                                    href="https://github.com/tushar-upadhya/admin"
-                                >
-                                    <img
-                                        src={github}
-                                        className="lg:h-12 lg:w-12 2xl:h-9 2xl:w-9 vvs:h-6 vvs:w-6 sm:w-7 sm:h-7 h-5 w-5 hover:-translate-y-1 transition duration-200  ease-in-out"
-                                    />
-                                </a>
-                            </div>
-                            <div className="liveDemo-btn">
-                                <a
-                                    className="icon"
-                                    target="_blank"
-                                    href="https://admin-tusharupadhyay.vercel.app/"
-                                >
-                                    <img
-                                        src={live}
-                                        alt="live image"
-                                        className="link lg:h-12 lg:w-12 2xl:h-6 2xl:w-6 vvs:h-5 vvs:w-5 sm:w-6 sm:h-6 h-4 w-4 hover:-translate-y-1 transition duration-200 ease-in-out"
-                                    />
-                                </a>
-                            </div>
-                        </div>
-                    </div>
-                </div> */}
-
         {/* PRM */}
 
-        <div className="container lg:space-x-2 flex flex-col-reverse lg:flex-row lg:justify-between lg:items-center items-start invisible">
+        {/* <div className="container lg:space-x-2 flex flex-col-reverse lg:flex-row lg:justify-between lg:items-center items-start invisible">
           <div className="text-container text-white lg:space-y-4 space-y-2 md:space-y-3 sm:space-y-3 ">
             <h1 className="hidden-text font-bold font-Glimer-Bold cursor-default lg:text-4xl text-2xl sm:text-3xl 2xl:text-5xl lg:mt-0 mt-5">
               <div className="content-line flex items-center m-0 lg:h-[52px] 2xl:h-[60px] overflow-hidden ">
@@ -221,14 +168,14 @@ const Projects = () => {
               className="absolute w-full h-full object-contain img"
             />
           </div>
-        </div>
+        </div> */}
 
-        {/* ADMIN */}
+        {/* PRM */}
 
         <div className="container lg:space-x-2 flex flex-col lg:flex-row lg:justify-between lg:items-center items-start invisible">
           <div className="lg:h-[400px] 2xl:h-[500px] h-[250px] sm:h-[300px] md:h-[350px] w-full relative overflow-hidden img-wrapper">
             <img
-              src={admin}
+              src={prm}
               alt="project image"
               className="absolute w-full h-full object-contain img"
             />
@@ -237,19 +184,20 @@ const Projects = () => {
           <div className="text-container text-white lg:space-y-4 space-y-2 md:space-y-3 sm:space-y-3">
             <h1 className="hidden-text cursor-default font-Glimer-Bold font-bold lg:text-4xl text-2xl sm:text-3xl 2xl:text-5xl lg:mt-0 mt-5">
               <div className="content-line flex lg:justify-end items-center m-0 lg:h-[52px] 2xl:h-[60px] overflow-hidden ">
-                <span>Admin</span>
+                <span>PRM</span>
               </div>
             </h1>
             <p className="font-normal cursor-default font-Glimer-Light text-white/90 xl:text-2xl sm:text-xl 2xl:text-3xl vvs:text-base text-sm lg:text-right">
-              An admin panel is focused on managing and updating a website or
-              application.
+              Customizable sections for users to define their preferred tech
+              stacks, social links, databases, frameworks libraries, and
+              programming languages.
             </p>
             <div className="btn-row flex justify-start space-x-6 items-center lg:justify-end">
               <div className="github-btn">
                 <a
                   className="icon "
                   target="_blank"
-                  href="https://github.com/tushar-upadhya/admin"
+                  href="https://github.com/tushar-upadhya/Profile-Read-Maker"
                 >
                   <img
                     src={github}
@@ -261,7 +209,7 @@ const Projects = () => {
                 <a
                   className="icon"
                   target="_blank"
-                  href="https://admin-tusharupadhyay.vercel.app/"
+                  href="https://prm-tusharupadhyay.vercel.app/"
                 >
                   <img
                     src={live}
@@ -380,24 +328,21 @@ const Projects = () => {
           </div>
         </div>
 
-        {/* youtube CLONE */}
+        {/* BEAT-X */}
 
         <div className="container lg:space-x-2 flex flex-col-reverse lg:flex-row lg:justify-between lg:items-center items-start invisible">
           <div className="text-container text-white lg:space-y-4 space-y-2 md:space-y-3 sm:space-y-3 ">
             <h1 className="hidden-text font-bold font-Glimer-Bold cursor-default lg:text-4xl text-2xl sm:text-3xl 2xl:text-5xl lg:mt-0 mt-5">
               <div className="content-line flex items-center m-0 lg:h-[52px] 2xl:h-[60px] overflow-hidden ">
                 <span>
-                  Youtube{" "}
-                  <span className="font-Glimer-Outlined font-semibold">
-                    CLONE
-                  </span>
+                  Beat{" "}
+                  <span className="font-Glimer-Outlined font-semibold">X</span>
                 </span>
               </div>
             </h1>
 
             <p className="font-normal cursor-default font-Glimer-Light text-white/90 xl:text-2xl  sm:text-xl 2xl:text-3xl vvs:text-base text-sm">
-              YouTube clone using Tailwind CSS, and ReactJS, replicating the
-              core functionalities of the original application.
+              A perfect Audio Store for Music Lover
             </p>
 
             <div className="btn-row flex justify-start space-x-6 items-center">
@@ -405,7 +350,7 @@ const Projects = () => {
                 <a
                   className="icon "
                   target="_blank"
-                  href="https://github.com/tushar-upadhya/youtube_clone-main"
+                  href="https://github.com/tushar-upadhya/beat-x"
                 >
                   <img
                     src={github}
@@ -417,7 +362,7 @@ const Projects = () => {
                 <a
                   className="icon"
                   target="_blank"
-                  href="https://youtube-clone-tusharupadhyay.netlify.app/"
+                  href="https://tushar-upadhyay-beat-x.vercel.app/"
                 >
                   <img
                     src={live}
@@ -429,11 +374,11 @@ const Projects = () => {
             </div>
           </div>
 
-          <div className="lg:h-[400px] 2xl:h-[500px] h-[250px] sm:h-[300px] md:h-[350px] w-full relative overflow-hidden img-wrapper">
+          <div className="lg:h-[400px] 2xl:h-[500px] h-[250px] sm:h-[300px] md:h-[350px] w-full relative overflow-hidden img-wrapper flex justify-end">
             <img
-              src={youtube}
-              alt="youtube image"
-              className="absolute w-full object-cover h-full img"
+              src={beat}
+              alt="beat-X image"
+              className="w-[350px] h-[350px] object-cover img"
             />
           </div>
         </div>
