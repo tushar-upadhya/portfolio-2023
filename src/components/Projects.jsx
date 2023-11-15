657bb365c0306834833a96f7d74d15c06437320e
tushar-upadhya <tusharupadhyay691@gmail.com>
2023-11-15 22:46:40 +0530

    feat: Add new words to cSpell dictionary and enable inline suggestions
    - Added new words to the cSpell dictionary for spell-checking.
    - Enabled inline suggestions in the code editor toolbar.
    
    The code changes include updating the "cSpell.words" array with
    additional words and adding the "editor.inlineSuggest.showToolbar"
    configuration to enable the display of suggestions on hover.

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -3,6 +3,7 @@ import { useEffect } from "react";
 import ecom from "../assets/images/projects/e-com.png";
 import mi from "../assets/images/projects/mi.png";
 import prm from "../assets/images/projects/readme.png";
+import beat from "../assets/images/projects/beat-x.png";
 
 import live from "../assets/images/icon/live2.png";
 import github from "../assets/images/icon/github.png";
@@ -114,61 +115,6 @@ const Projects = () => {
       <div className="sm:space-y-40 space-y-32 xl:space-y-[35vh] z-30 ">
         {/* PRM */}
 
-        {/* <div className="container lg:space-x-2 flex flex-col-reverse lg:flex-row lg:justify-between lg:items-center items-start invisible">
-          <div className="text-container text-white lg:space-y-4 space-y-2 md:space-y-3 sm:space-y-3 ">
-            <h1 className="hidden-text font-bold font-Glimer-Bold cursor-default lg:text-4xl text-2xl sm:text-3xl 2xl:text-5xl lg:mt-0 mt-5">
-              <div className="content-line flex items-center m-0 lg:h-[52px] 2xl:h-[60px] overflow-hidden ">
-                <span>PRM</span>
-              </div>
-            </h1>
-
-            <p className="font-normal cursor-default font-Glimer-Light text-white/90 xl:text-2xl  sm:text-xl 2xl:text-3xl vvs:text-base text-sm">
-              Customizable sections for users to define their preferred tech
-              stacks, social links, databases, frameworks libraries, and
-              programming languages.
-            </p>
-
-            <div className="btn-row flex justify-start space-x-6 items-center">
-              <div className="github-btn">
-                <a
-                  className="icon "
-                  target="_blank"
-                  href="https://github.com/tushar-upadhya/Profile-Read-Maker"
-                >
-                  <img
-                    src={github}
-                    className="lg:h-12 lg:w-12 2xl:h-9 2xl:w-9 vvs:h-6 vvs:w-6 sm:w-7 sm:h-7 h-5 w-5 hover:-translate-y-1 transition duration-200  ease-in-out"
-                  />
-                </a>
-              </div>
-
-              <div className="liveDemo-btn">
-                <a
-                  className="icon"
-                  target="_blank"
-                  href="https://prm-tusharupadhyay.vercel.app/"
-                >
-                  <img
-                    src={live}
-                    alt="live image"
-                    className="link lg:h-12 lg:w-12  2xl:h-9 2xl:w-9 vvs:h-6 vvs:w-6 sm:w-7 sm:h-7 h-5 w-5 hover:-translate-y-1 transition duration-200  ease-in-out"
-                  />
-                </a>
-              </div>
-            </div>
-          </div>
-
-          <div className="lg:h-[400px] 2xl:h-[500px] h-[250px] sm:h-[300px] md:h-[350px] w-full relative overflow-hidden img-wrapper">
-            <img
-              src={prm}
-              alt="project image"
-              className="absolute w-full h-full object-contain img"
-            />
-          </div>
-        </div> */}
-
-        {/* PRM */}
-
         <div className="container lg:space-x-2 flex flex-col lg:flex-row lg:justify-between lg:items-center items-start invisible">
           <div className="lg:h-[400px] 2xl:h-[500px] h-[250px] sm:h-[300px] md:h-[350px] w-full relative overflow-hidden img-wrapper">
             <img
@@ -269,12 +215,12 @@ const Projects = () => {
           </div>
         </div>
 
-        {/* MI Store CLONE */}
+        {/* Beat - x */}
 
         <div className="container lg:space-x-2 flex flex-col lg:flex-row lg:justify-between lg:items-center items-start invisible">
           <div className="lg:h-[400px] 2xl:h-[500px] h-[250px] sm:h-[300px] md:h-[350px] w-full relative overflow-hidden img-wrapper">
             <img
-              src={mi}
+              src={beat}
               alt="project image"
               className="absolute w-full h-full object-contain img"
             />
@@ -283,27 +229,69 @@ const Projects = () => {
           <div className="text-container text-white lg:space-y-4 space-y-2 md:space-y-3 sm:space-y-3">
             <h1 className="hidden-text cursor-default font-Glimer-Bold font-bold lg:text-4xl text-2xl sm:text-3xl 2xl:text-5xl lg:mt-0 mt-5">
               <div className="content-line flex lg:justify-end items-center m-0 lg:h-[52px] 2xl:h-[60px] overflow-hidden ">
-                <span>
-                  MI Store{" "}
-                  <span className="font-Glimer-Outlined font-semibold">
-                    CLONE
-                  </span>
-                </span>{" "}
+                <span>Beat- </span>
+                <span className="font-Glimer-Outlined font-semibold">X</span>
               </div>
             </h1>
             <p className="font-normal cursor-default font-Glimer-Light text-white/90 xl:text-2xl sm:text-xl 2xl:text-3xl vvs:text-base text-sm lg:text-right">
-              A perfect Electronics Store for Tech Lovers
+              A perfect Electronics Store for Music Lovers
             </p>
             <div className="btn-row flex justify-start space-x-6 items-center lg:justify-end">
               <div className="github-btn">
                 <a
                   className="icon "
                   target="_blank"
-                  href="https://github.com/tushar-upadhya/mi-store"
+                  href="https://github.com/tushar-upadhya/beat-x"
                 >
                   <img
                     src={github}
                     className="lg:h-12 lg:w-12 2xl:h-9 2xl:w-9 vvs:h-6 vvs:w-6 sm:w-7 sm:h-7 h-5 w-5 hover:-translate-y-1 transition duration-200  ease-in-out"
+                  />
+                </a>
+              </div>
+              <div className="liveDemo-btn">
+                <a
+                  className="icon"
+                  target="_blank"
+                  href="https://tushar-upadhyay-beat-x.vercel.app/"
+                >
+                  <img
+                    src={live}
+                    alt="live image"
+                    className="lg:h-12 lg:w-12 2xl:h-6 2xl:w-6 vvs:h-5 vvs:w-5 sm:w-6 sm:h-6 h-4 w-4 hover:-translate-y-1 transition duration-200 ease-in-out"
+                  />
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+        {/* MI Store CLONE */}
+
+        <div className="container lg:space-x-2 flex flex-col-reverse lg:flex-row lg:justify-between lg:items-center items-start invisible">
+          <div className="text-container text-white lg:space-y-4 space-y-2 md:space-y-3 sm:space-y-3">
+            <h1 className="hidden-text font-bold font-Glimer-Bold cursor-default lg:text-4xl text-2xl sm:text-3xl 2xl:text-5xl lg:mt-0 mt-5">
+              <div className="content-line flex items-center m-0 lg:h-[52px] 2xl:h-[60px] overflow-hidden">
+                <span>MI-</span>
+                {"  "}
+                <span className="font-Glimer-Outlined font-semibold">
+                  {"  "}
+                  CLONE
+                </span>
+              </div>
+            </h1>
+            <p className="font-normal cursor-default font-Glimer-Light text-white/90 xl:text-2xl sm:text-xl 2xl:text-3xl vvs:text-base text-sm">
+              This is a clone of the official website of MI India.
+            </p>
+            <div className="btn-row flex justify-start space-x-6 items-center">
+              <div className="github-btn">
+                <a
+                  className="icon"
+                  target="_blank"
+                  href="https://github.com/tushar-upadhya/mi-store"
+                >
+                  <img
+                    src={github}
+                    className="lg:h-12 lg:w-12 2xl:h-9 2xl:w-9 vvs:h-6 vvs:w-6 sm:w-7 sm:h-7 h-5 w-5 hover:-translate-y-1 transition duration-200 ease-in-out"
                   />
                 </a>
               </div>
@@ -316,11 +304,18 @@ const Projects = () => {
                   <img
                     src={live}
                     alt="live image"
-                    className="lg:h-12 lg:w-12 2xl:h-6 2xl:w-6 vvs:h-5 vvs:w-5 sm:w-6 sm:h-6 h-4 w-4 hover:-translate-y-1 transition duration-200 ease-in-out"
+                    className="link lg:h-12 lg:w-12 2xl:h-9 2xl:w-9 vvs:h-6 vvs:w-6 sm:w-7 sm:h-7 h-5 w-5 hover:-translate-y-1 transition duration-200 ease-in-out"
                   />
                 </a>
               </div>
             </div>
+          </div>
+          <div className="lg:h-[400px] 2xl:h-[500px] h-[250px] sm:h-[300px] md:h-[350px] w-full relative overflow-hidden img-wrapper">
+            <img
+              src={mi}
+              alt="project image"
+              className="absolute w-full h-full object-contain img"
+            />
           </div>
         </div>
       </div>
