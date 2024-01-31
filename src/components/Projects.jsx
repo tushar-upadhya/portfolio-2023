8c6e20d3917ac9d4c1512f35d61120801f81eb24
tushar-upadhya <tusharupadhyay691@gmail.com>
2024-01-31 09:21:29 +0530

    projects and CV

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -3,7 +3,9 @@ import { useEffect } from "react";
 import ecom from "../assets/images/projects/e-com.png";
 import mi from "../assets/images/projects/mi.png";
 import prm from "../assets/images/projects/readme.png";
-import codeToImage from "../assets/images/projects/codeToImage.jpg";
+import codeToImage from "../assets/images/projects/codeToImage.png";
+import laptop from "../assets/images/projects/laptop.png";
+import gita from "../assets/images/projects/gita.png";
 
 import live from "../assets/images/icon/live2.png";
 import github from "../assets/images/icon/github.png";
@@ -106,7 +108,7 @@ const Projects = () => {
 
   return (
     <div
-      className="w-full pb-10 xl:px-44 lg:px-36 md:px-32 sm:px-28 vvs:px-12 px-10"
+      className="w-full px-10 pb-10 xl:px-44 lg:px-36 md:px-32 sm:px-28 vvs:px-12"
       id="projects"
     >
       <h1 className="text-white/70 font-Glimer-Outlined font-semibold z-40 lg:mb-32 mb-20 vvs:text-[4rem] sm:text-[5.2rem] md:text-[6.4rem] lg:text-[7.5rem] xl:text-[9.2rem] 2xl:text-[10.8rem]  text-[3.5rem] leading-[3.5rem] sm:leading-[5.5rem] md:leading-[6rem] lg:leading-[7rem] xl:leading-[9rem] 2xl:leading-[10rem]">
@@ -115,27 +117,26 @@ const Projects = () => {
 
       <div className="sm:space-y-40 space-y-32 xl:space-y-[35vh] z-30 ">
         {/* code-to-image */}
-        <div className="container lg:space-x-2 flex flex-col-reverse lg:flex-row lg:justify-between lg:items-center items-start invisible">
-          <div className="text-container text-white lg:space-y-4 space-y-2 md:space-y-3 sm:space-y-3">
-            <h1 className="hidden-text font-bold font-Glimer-Bold cursor-default lg:text-4xl text-2xl sm:text-3xl 2xl:text-5xl lg:mt-0 mt-5">
+        <div className="container flex flex-col-reverse items-start invisible lg:space-x-2 lg:flex-row lg:justify-between lg:items-center">
+          <div className="space-y-2 text-white text-container lg:space-y-4 md:space-y-3 sm:space-y-3">
+            <h1 className="mt-5 text-2xl font-bold cursor-default hidden-text font-Glimer-Bold lg:text-4xl sm:text-3xl 2xl:text-5xl lg:mt-0">
               <div className="content-line flex items-center m-0 lg:h-[52px] 2xl:h-[60px] overflow-hidden">
-                <span>Code To Image </span>
+                <span>Bhagavad Gita </span>
               </div>
             </h1>
-            <p className="font-normal cursor-default font-Glimer-Light text-white/90 xl:text-2xl sm:text-xl 2xl:text-3xl vvs:text-base text-sm">
-              E-commerce website, Payment gateway and user-friendly shopping
-              experience.
+            <p className="text-sm font-normal cursor-default font-Glimer-Light text-white/90 xl:text-2xl sm:text-xl 2xl:text-3xl vvs:text-base">
+              Most Powerful text book with model UI
             </p>
-            <div className="btn-row flex justify-start space-x-6 items-center">
+            <div className="flex items-center justify-start space-x-6 btn-row">
               <div className="github-btn">
                 <a
                   className="icon"
                   target="_blank"
-                  href="https://github.com/tushar-upadhya/e-com-full"
+                  href="https://github.com/tushar-upadhya/Bhagavad-Gita"
                 >
                   <img
                     src={github}
-                    className="lg:h-12 lg:w-12 2xl:h-9 2xl:w-9 vvs:h-6 vvs:w-6 sm:w-7 sm:h-7 h-5 w-5 hover:-translate-y-1 transition duration-200 ease-in-out"
+                    className="w-5 h-5 transition duration-200 ease-in-out lg:h-12 lg:w-12 2xl:h-9 2xl:w-9 vvs:h-6 vvs:w-6 sm:w-7 sm:h-7 hover:-translate-y-1"
                   />
                 </a>
               </div>
@@ -143,12 +144,113 @@ const Projects = () => {
                 <a
                   className="icon"
                   target="_blank"
-                  href="https://bummer-tusharupadhyay.vercel.app/"
+                  href="https://bhagavad-gita-tusharupadhyay.vercel.app/"
                 >
                   <img
                     src={live}
                     alt="live image"
-                    className="link lg:h-12 lg:w-12 2xl:h-9 2xl:w-9 vvs:h-6 vvs:w-6 sm:w-7 sm:h-7 h-5 w-5 hover:-translate-y-1 transition duration-200 ease-in-out"
+                    className="w-5 h-5 transition duration-200 ease-in-out link lg:h-12 lg:w-12 2xl:h-9 2xl:w-9 vvs:h-6 vvs:w-6 sm:w-7 sm:h-7 hover:-translate-y-1"
+                  />
+                </a>
+              </div>
+            </div>
+          </div>
+          <div className="lg:h-[400px] 2xl:h-[500px] h-[250px] sm:h-[300px] md:h-[350px] w-full relative overflow-hidden img-wrapper">
+            <img
+              src={gita}
+              alt="project image"
+              className="absolute object-contain w-full h-full img"
+            />
+          </div>
+        </div>
+
+        {/* Writer Pro */}
+
+        <div className="container flex flex-col items-start invisible lg:space-x-2 lg:flex-row lg:justify-between lg:items-center">
+          <div className="lg:h-[400px] 2xl:h-[500px] h-[250px] sm:h-[300px] md:h-[350px] w-full relative overflow-hidden img-wrapper">
+            <img
+              src={laptop}
+              alt="project image"
+              className="absolute object-contain w-full h-full img"
+            />
+          </div>
+
+          <div className="space-y-2 text-white text-container lg:space-y-4 md:space-y-3 sm:space-y-3">
+            <h1 className="mt-5 text-2xl font-bold cursor-default hidden-text font-Glimer-Bold lg:text-4xl sm:text-3xl 2xl:text-5xl lg:mt-0">
+              <div className="content-line flex lg:justify-end items-center m-0 lg:h-[52px] 2xl:h-[60px] overflow-hidden ">
+                <span>Writer Pro</span>
+              </div>
+            </h1>
+            <p className="text-sm font-normal cursor-default font-Glimer-Light text-white/90 xl:text-2xl sm:text-xl 2xl:text-3xl vvs:text-base lg:text-right">
+              You can see letter count, word count, line count, and text memory
+              size in Statistics section. You can also set text size according
+              to your requirement and of your choice.
+            </p>
+            <div className="flex items-center justify-start space-x-6 btn-row lg:justify-end">
+              <div className="github-btn">
+                <a
+                  className="icon "
+                  target="_blank"
+                  href="https://github.com/tushar-upadhya/writer-pro"
+                >
+                  <img
+                    src={github}
+                    className="w-5 h-5 transition duration-200 ease-in-out lg:h-12 lg:w-12 2xl:h-9 2xl:w-9 vvs:h-6 vvs:w-6 sm:w-7 sm:h-7 hover:-translate-y-1"
+                  />
+                </a>
+              </div>
+              <div className="liveDemo-btn">
+                <a
+                  className="icon"
+                  target="_blank"
+                  href="https://writerpro-tushar-upadhyay.vercel.app/"
+                >
+                  <img
+                    src={live}
+                    alt="live image"
+                    className="w-4 h-4 transition duration-200 ease-in-out lg:h-12 lg:w-12 2xl:h-6 2xl:w-6 vvs:h-5 vvs:w-5 sm:w-6 sm:h-6 hover:-translate-y-1"
+                  />
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        {/* code-to-image */}
+        <div className="container flex flex-col-reverse items-start invisible lg:space-x-2 lg:flex-row lg:justify-between lg:items-center">
+          <div className="space-y-2 text-white text-container lg:space-y-4 md:space-y-3 sm:space-y-3">
+            <h1 className="mt-5 text-2xl font-bold cursor-default hidden-text font-Glimer-Bold lg:text-4xl sm:text-3xl 2xl:text-5xl lg:mt-0">
+              <div className="content-line flex items-center m-0 lg:h-[52px] 2xl:h-[60px] overflow-hidden">
+                <span>Code To Image </span>
+              </div>
+            </h1>
+            <p className="text-sm font-normal cursor-default font-Glimer-Light text-white/90 xl:text-2xl sm:text-xl 2xl:text-3xl vvs:text-base">
+              A web app, allowing tech creators to showcase their code in a
+              visually appealing format.
+            </p>
+            <div className="flex items-center justify-start space-x-6 btn-row">
+              <div className="github-btn">
+                <a
+                  className="icon"
+                  target="_blank"
+                  href="https://github.com/tushar-upadhya/code2img"
+                >
+                  <img
+                    src={github}
+                    className="w-5 h-5 transition duration-200 ease-in-out lg:h-12 lg:w-12 2xl:h-9 2xl:w-9 vvs:h-6 vvs:w-6 sm:w-7 sm:h-7 hover:-translate-y-1"
+                  />
+                </a>
+              </div>
+              <div className="liveDemo-btn">
+                <a
+                  className="icon"
+                  target="_blank"
+                  href="https://code2img-tusharupadhyay.vercel.app/"
+                >
+                  <img
+                    src={live}
+                    alt="live image"
+                    className="w-5 h-5 transition duration-200 ease-in-out link lg:h-12 lg:w-12 2xl:h-9 2xl:w-9 vvs:h-6 vvs:w-6 sm:w-7 sm:h-7 hover:-translate-y-1"
                   />
                 </a>
               </div>
@@ -158,34 +260,34 @@ const Projects = () => {
             <img
               src={codeToImage}
               alt="project image"
-              className="absolute w-full h-full object-contain img"
+              className="absolute object-contain w-full h-full img"
             />
           </div>
         </div>
 
         {/* PRM */}
 
-        <div className="container lg:space-x-2 flex flex-col lg:flex-row lg:justify-between lg:items-center items-start invisible">
+        <div className="container flex flex-col items-start invisible lg:space-x-2 lg:flex-row lg:justify-between lg:items-center">
           <div className="lg:h-[400px] 2xl:h-[500px] h-[250px] sm:h-[300px] md:h-[350px] w-full relative overflow-hidden img-wrapper">
             <img
               src={prm}
               alt="project image"
-              className="absolute w-full h-full object-contain img"
+              className="absolute object-contain w-full h-full img"
             />
           </div>
 
-          <div className="text-container text-white lg:space-y-4 space-y-2 md:space-y-3 sm:space-y-3">
-            <h1 className="hidden-text cursor-default font-Glimer-Bold font-bold lg:text-4xl text-2xl sm:text-3xl 2xl:text-5xl lg:mt-0 mt-5">
+          <div className="space-y-2 text-white text-container lg:space-y-4 md:space-y-3 sm:space-y-3">
+            <h1 className="mt-5 text-2xl font-bold cursor-default hidden-text font-Glimer-Bold lg:text-4xl sm:text-3xl 2xl:text-5xl lg:mt-0">
               <div className="content-line flex lg:justify-end items-center m-0 lg:h-[52px] 2xl:h-[60px] overflow-hidden ">
                 <span>PRM</span>
               </div>
             </h1>
-            <p className="font-normal cursor-default font-Glimer-Light text-white/90 xl:text-2xl sm:text-xl 2xl:text-3xl vvs:text-base text-sm lg:text-right">
+            <p className="text-sm font-normal cursor-default font-Glimer-Light text-white/90 xl:text-2xl sm:text-xl 2xl:text-3xl vvs:text-base lg:text-right">
               Customizable sections for users to define their preferred tech
               stacks, social links, databases, frameworks libraries, and
               programming languages.
             </p>
-            <div className="btn-row flex justify-start space-x-6 items-center lg:justify-end">
+            <div className="flex items-center justify-start space-x-6 btn-row lg:justify-end">
               <div className="github-btn">
                 <a
                   className="icon "
@@ -194,7 +296,7 @@ const Projects = () => {
                 >
                   <img
                     src={github}
-                    className="lg:h-12 lg:w-12 2xl:h-9 2xl:w-9 vvs:h-6 vvs:w-6 sm:w-7 sm:h-7 h-5 w-5 hover:-translate-y-1 transition duration-200  ease-in-out"
+                    className="w-5 h-5 transition duration-200 ease-in-out lg:h-12 lg:w-12 2xl:h-9 2xl:w-9 vvs:h-6 vvs:w-6 sm:w-7 sm:h-7 hover:-translate-y-1"
                   />
                 </a>
               </div>
@@ -207,7 +309,7 @@ const Projects = () => {
                   <img
                     src={live}
                     alt="live image"
-                    className="lg:h-12 lg:w-12 2xl:h-6 2xl:w-6 vvs:h-5 vvs:w-5 sm:w-6 sm:h-6 h-4 w-4 hover:-translate-y-1 transition duration-200 ease-in-out"
+                    className="w-4 h-4 transition duration-200 ease-in-out lg:h-12 lg:w-12 2xl:h-6 2xl:w-6 vvs:h-5 vvs:w-5 sm:w-6 sm:h-6 hover:-translate-y-1"
                   />
                 </a>
               </div>
@@ -217,18 +319,18 @@ const Projects = () => {
 
         {/*  BUMMER */}
 
-        <div className="container lg:space-x-2 flex flex-col-reverse lg:flex-row lg:justify-between lg:items-center items-start invisible">
-          <div className="text-container text-white lg:space-y-4 space-y-2 md:space-y-3 sm:space-y-3">
-            <h1 className="hidden-text font-bold font-Glimer-Bold cursor-default lg:text-4xl text-2xl sm:text-3xl 2xl:text-5xl lg:mt-0 mt-5">
+        <div className="container flex flex-col-reverse items-start invisible lg:space-x-2 lg:flex-row lg:justify-between lg:items-center">
+          <div className="space-y-2 text-white text-container lg:space-y-4 md:space-y-3 sm:space-y-3">
+            <h1 className="mt-5 text-2xl font-bold cursor-default hidden-text font-Glimer-Bold lg:text-4xl sm:text-3xl 2xl:text-5xl lg:mt-0">
               <div className="content-line flex items-center m-0 lg:h-[52px] 2xl:h-[60px] overflow-hidden">
                 <span>Bummer </span>
               </div>
             </h1>
-            <p className="font-normal cursor-default font-Glimer-Light text-white/90 xl:text-2xl sm:text-xl 2xl:text-3xl vvs:text-base text-sm">
+            <p className="text-sm font-normal cursor-default font-Glimer-Light text-white/90 xl:text-2xl sm:text-xl 2xl:text-3xl vvs:text-base">
               E-commerce website, Payment gateway and user-friendly shopping
               experience.
             </p>
-            <div className="btn-row flex justify-start space-x-6 items-center">
+            <div className="flex items-center justify-start space-x-6 btn-row">
               <div className="github-btn">
                 <a
                   className="icon"
@@ -237,7 +339,7 @@ const Projects = () => {
                 >
                   <img
                     src={github}
-                    className="lg:h-12 lg:w-12 2xl:h-9 2xl:w-9 vvs:h-6 vvs:w-6 sm:w-7 sm:h-7 h-5 w-5 hover:-translate-y-1 transition duration-200 ease-in-out"
+                    className="w-5 h-5 transition duration-200 ease-in-out lg:h-12 lg:w-12 2xl:h-9 2xl:w-9 vvs:h-6 vvs:w-6 sm:w-7 sm:h-7 hover:-translate-y-1"
                   />
                 </a>
               </div>
@@ -250,7 +352,7 @@ const Projects = () => {
                   <img
                     src={live}
                     alt="live image"
-                    className="link lg:h-12 lg:w-12 2xl:h-9 2xl:w-9 vvs:h-6 vvs:w-6 sm:w-7 sm:h-7 h-5 w-5 hover:-translate-y-1 transition duration-200 ease-in-out"
+                    className="w-5 h-5 transition duration-200 ease-in-out link lg:h-12 lg:w-12 2xl:h-9 2xl:w-9 vvs:h-6 vvs:w-6 sm:w-7 sm:h-7 hover:-translate-y-1"
                   />
                 </a>
               </div>
@@ -260,29 +362,24 @@ const Projects = () => {
             <img
               src={ecom}
               alt="project image"
-              className="absolute w-full h-full object-contain img"
+              className="absolute object-contain w-full h-full img"
             />
           </div>
         </div>
 
         {/* MI Store CLONE */}
 
-        <div className="container lg:space-x-2 flex flex-col-reverse lg:flex-row lg:justify-between lg:items-center items-start invisible">
-          <div className="text-container text-white lg:space-y-4 space-y-2 md:space-y-3 sm:space-y-3">
-            <h1 className="hidden-text font-bold font-Glimer-Bold cursor-default lg:text-4xl text-2xl sm:text-3xl 2xl:text-5xl lg:mt-0 mt-5">
+        <div className="container flex flex-col-reverse items-start invisible lg:space-x-2 lg:flex-row lg:justify-between lg:items-center">
+          <div className="space-y-2 text-white text-container lg:space-y-4 md:space-y-3 sm:space-y-3">
+            <h1 className="mt-5 text-2xl font-bold cursor-default hidden-text font-Glimer-Bold lg:text-4xl sm:text-3xl 2xl:text-5xl lg:mt-0">
               <div className="content-line flex items-center m-0 lg:h-[52px] 2xl:h-[60px] overflow-hidden">
-                <span>MI-</span>
-                {"  "}
-                <span className="font-Glimer-Outlined font-semibold">
-                  {"  "}
-                  CLONE
-                </span>
+                <span>MI-Clone</span>
               </div>
             </h1>
-            <p className="font-normal cursor-default font-Glimer-Light text-white/90 xl:text-2xl sm:text-xl 2xl:text-3xl vvs:text-base text-sm">
+            <p className="text-sm font-normal cursor-default font-Glimer-Light text-white/90 xl:text-2xl sm:text-xl 2xl:text-3xl vvs:text-base">
               This is a clone of the official website of MI India.
             </p>
-            <div className="btn-row flex justify-start space-x-6 items-center">
+            <div className="flex items-center justify-start space-x-6 btn-row">
               <div className="github-btn">
                 <a
                   className="icon"
@@ -291,7 +388,7 @@ const Projects = () => {
                 >
                   <img
                     src={github}
-                    className="lg:h-12 lg:w-12 2xl:h-9 2xl:w-9 vvs:h-6 vvs:w-6 sm:w-7 sm:h-7 h-5 w-5 hover:-translate-y-1 transition duration-200 ease-in-out"
+                    className="w-5 h-5 transition duration-200 ease-in-out lg:h-12 lg:w-12 2xl:h-9 2xl:w-9 vvs:h-6 vvs:w-6 sm:w-7 sm:h-7 hover:-translate-y-1"
                   />
                 </a>
               </div>
@@ -304,7 +401,7 @@ const Projects = () => {
                   <img
                     src={live}
                     alt="live image"
-                    className="link lg:h-12 lg:w-12 2xl:h-9 2xl:w-9 vvs:h-6 vvs:w-6 sm:w-7 sm:h-7 h-5 w-5 hover:-translate-y-1 transition duration-200 ease-in-out"
+                    className="w-5 h-5 transition duration-200 ease-in-out link lg:h-12 lg:w-12 2xl:h-9 2xl:w-9 vvs:h-6 vvs:w-6 sm:w-7 sm:h-7 hover:-translate-y-1"
                   />
                 </a>
               </div>
@@ -314,7 +411,7 @@ const Projects = () => {
             <img
               src={mi}
               alt="project image"
-              className="absolute w-full h-full object-contain img"
+              className="absolute object-contain w-full h-full img"
             />
           </div>
         </div>
