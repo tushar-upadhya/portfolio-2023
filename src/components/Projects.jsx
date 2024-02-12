68de60346bc4d1c5989f7164af136590824b92f9
tushar-upadhya <tusharupadhyay691@gmail.com>
2024-02-12 18:50:09 +0530

    project

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,11 +1,11 @@
 import { useEffect } from "react";
 
 import ecom from "../assets/images/projects/e-com.png";
-import mi from "../assets/images/projects/mi.png";
 import prm from "../assets/images/projects/readme.png";
 import codeToImage from "../assets/images/projects/codeToImage.png";
 import laptop from "../assets/images/projects/laptop.png";
 import gita from "../assets/images/projects/gita.png";
+import chat from "../assets/images/projects/chat.png";
 
 import live from "../assets/images/icon/live2.png";
 import github from "../assets/images/icon/github.png";
@@ -116,7 +116,61 @@ const Projects = () => {
       </h1>
 
       <div className="sm:space-y-40 space-y-32 xl:space-y-[35vh] z-30 ">
-        {/* code-to-image */}
+        {/* Writer Pro */}
+
+        <div className="container flex flex-col items-start invisible lg:space-x-2 lg:flex-row lg:justify-between lg:items-center">
+          <div className="lg:h-[400px] 2xl:h-[500px] h-[250px] sm:h-[300px] md:h-[350px] w-full relative overflow-hidden img-wrapper">
+            <img
+              src={chat}
+              alt="project image"
+              className="absolute object-contain w-full h-full img"
+            />
+          </div>
+
+          <div className="space-y-2 text-white text-container lg:space-y-4 md:space-y-3 sm:space-y-3">
+            <h1 className="mt-5 text-2xl font-bold cursor-default hidden-text font-Glimer-Bold lg:text-4xl sm:text-3xl 2xl:text-5xl lg:mt-0">
+              <div className="content-line flex lg:justify-end items-center m-0 lg:h-[52px] 2xl:h-[60px] overflow-hidden ">
+                <span>Chat 2 Chat</span>
+              </div>
+            </h1>
+
+            <p className="text-sm font-normal cursor-default font-Glimer-Light text-white/90 xl:text-2xl sm:text-xl 2xl:text-3xl vvs:text-base lg:text-right">
+              It's built with end-to-end encryption technology and designed to
+              be responsive on all devices.
+            </p>
+
+            <div className="flex items-center justify-start space-x-6 btn-row lg:justify-end">
+              <div className="github-btn">
+                <a
+                  className="icon "
+                  target="_blank"
+                  href="https://github.com/tushar-upadhya/chatt"
+                >
+                  <img
+                    src={github}
+                    className="w-5 h-5 transition duration-200 ease-in-out lg:h-12 lg:w-12 2xl:h-9 2xl:w-9 vvs:h-6 vvs:w-6 sm:w-7 sm:h-7 hover:-translate-y-1"
+                  />
+                </a>
+              </div>
+
+              <div className="liveDemo-btn">
+                <a
+                  className="icon"
+                  target="_blank"
+                  href="https://chat-tusharupadhyay.vercel.app/"
+                >
+                  <img
+                    src={live}
+                    alt="live image"
+                    className="w-4 h-4 transition duration-200 ease-in-out lg:h-12 lg:w-12 2xl:h-6 2xl:w-6 vvs:h-5 vvs:w-5 sm:w-6 sm:h-6 hover:-translate-y-1"
+                  />
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        {/* Bhagavad Gita  */}
         <div className="container flex flex-col-reverse items-start invisible lg:space-x-2 lg:flex-row lg:justify-between lg:items-center">
           <div className="space-y-2 text-white text-container lg:space-y-4 md:space-y-3 sm:space-y-3">
             <h1 className="mt-5 text-2xl font-bold cursor-default hidden-text font-Glimer-Bold lg:text-4xl sm:text-3xl 2xl:text-5xl lg:mt-0">
@@ -366,55 +420,6 @@ const Projects = () => {
           <div className="lg:h-[400px] 2xl:h-[500px] h-[250px] sm:h-[300px] md:h-[350px] w-full relative overflow-hidden img-wrapper">
             <img
               src={ecom}
-              alt="project image"
-              className="absolute object-contain w-full h-full img"
-            />
-          </div>
-        </div>
-
-        {/* MI Store CLONE */}
-
-        <div className="container flex flex-col-reverse items-start invisible lg:space-x-2 lg:flex-row lg:justify-between lg:items-center">
-          <div className="space-y-2 text-white text-container lg:space-y-4 md:space-y-3 sm:space-y-3">
-            <h1 className="mt-5 text-2xl font-bold cursor-default hidden-text font-Glimer-Bold lg:text-4xl sm:text-3xl 2xl:text-5xl lg:mt-0">
-              <div className="content-line flex items-center m-0 lg:h-[52px] 2xl:h-[60px] overflow-hidden">
-                <span>MI-Clone</span>
-              </div>
-            </h1>
-            <p className="text-sm font-normal cursor-default font-Glimer-Light text-white/90 xl:text-2xl sm:text-xl 2xl:text-3xl vvs:text-base">
-              This is a clone of the official website of MI India.
-            </p>
-            <div className="flex items-center justify-start space-x-6 btn-row">
-              <div className="github-btn">
-                <a
-                  className="icon"
-                  target="_blank"
-                  href="https://github.com/tushar-upadhya/mi-store"
-                >
-                  <img
-                    src={github}
-                    className="w-5 h-5 transition duration-200 ease-in-out lg:h-12 lg:w-12 2xl:h-9 2xl:w-9 vvs:h-6 vvs:w-6 sm:w-7 sm:h-7 hover:-translate-y-1"
-                  />
-                </a>
-              </div>
-              <div className="liveDemo-btn">
-                <a
-                  className="icon"
-                  target="_blank"
-                  href="https://mi-store-tusharupadhyay.netlify.app/"
-                >
-                  <img
-                    src={live}
-                    alt="live image"
-                    className="w-5 h-5 transition duration-200 ease-in-out link lg:h-12 lg:w-12 2xl:h-9 2xl:w-9 vvs:h-6 vvs:w-6 sm:w-7 sm:h-7 hover:-translate-y-1"
-                  />
-                </a>
-              </div>
-            </div>
-          </div>
-          <div className="lg:h-[400px] 2xl:h-[500px] h-[250px] sm:h-[300px] md:h-[350px] w-full relative overflow-hidden img-wrapper">
-            <img
-              src={mi}
               alt="project image"
               className="absolute object-contain w-full h-full img"
             />
